02df678fbb4727b23e833ca974a5007d4c5f1bf3
石ころ <105261164+isikoro1@users.noreply.github.com>
2025-09-08 12:11:09 +0900

    add ABC422A.java

---
+++ codes/AtCoder_2025/ABC422A.java
@@ -0,0 +1,27 @@
+package AtCoder_2025;
+import java.util.*;
+
+class ABC422A {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] S = sc.nextLine().split("-");
+        int n = Integer.parseInt(S[0]); 
+        int m = Integer.parseInt(S[1]);   
+
+
+        if (n == 8) {
+            m++;
+            System.out.println(n + "-" + m);
+        } else {
+            if (m == 8) {
+                m = 1;
+                n++;
+                System.out.println(n + "-" + m);
+            } else {
+                m++;
+                System.out.println(n + "-" + m);
+            }
+        }
+
+    }
+}
